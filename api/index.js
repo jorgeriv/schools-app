8c5233e7ea8542909aeeb58acdc643461d96d506
Jorge Rivera <jorgeriv@gmail.com>
2019-08-04 19:30:20 -0500

    add middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
 const app = express();
 const port = 3001;
+
+app.use(bodyParser.json());
+app.use(multer.none());
 
 app.get('/', (req, res) => res.send("Hello World!"));
 
